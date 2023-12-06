f25361e224689c81d804d142dbdade856f4da39c
Sahibzada Hamza sardar <71062903+hamzoo124@users.noreply.github.com>
2023-12-06 17:53:00 +0500

    changed the db.js to use .env variables

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,14 @@
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('Flone', 'postgres', 'hamza', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
+// module.exports = sequelize;
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('ecommerce', 'postgres', '123', {
-  host: 'localhost',
-  dialect: 'postgres'
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT
 });
+
 module.exports = sequelize;
