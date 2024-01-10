2e93b38895bc4fb1aab8ef183ed4d87b3eac08d1
luthfullah <luthfullah111@gmail.com>
2024-01-10 17:51:36 +0500

    login, middleware, productImages changes done

--- server/routes/productImages.js
+++ server/routes/productImages.js
@@ -3,35 +3,33 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const productImages = require('../models/productImages');
+const { upload } = require('../middlewares/multerMiddleware');
 
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, process.env.DESTINATION_PATH_FOR_IMAGES);//env
+//   },
+//   filename: (req, file, cb) => {
+//    const name=file.originalname;
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//     cb(null, 'image' + '-' +uniqueSuffix+ '-' +name);
+//   },
+// });
 
+// const upload = multer({ 
+//   storage: storage,
+//   limits: { fileSize: process.env.FILE_SIZE },//env
+//   fileFilter: (req, file, cb) => {
+//     const fileTypes = /jpeg|jpg|png|gif/;
+//     const mimeType = fileTypes.test(file.mimetype);
+//     const extname = fileTypes.test(path.extname(file.originalname));
 
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, '/assets/products');
-  },
-  filename: (req, file, cb) => {
-   const name=file.originalname;
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, name);
-  },
-});
-
-const upload = multer({ 
-  storage: storage,
-  limits: { fileSize: 100000000 },
-  fileFilter: (req, file, cb) => {
-    const fileTypes = /jpeg|jpg|png|gif/;
-    const mimeType = fileTypes.test(file.mimetype);
-    const extname = fileTypes.test(path.extname(file.originalname));
-
-    if (mimeType && extname) {
-      return cb(null, true);
-    }
-    cb('Give proper file');
-  }
-}).array('images', 3); 
+//     if (mimeType && extname) {
+//       return cb(null, true);
+//     }
+//     cb('Give proper file');
+//   }
+// }).array('images', process.env.IMAGE_LIMIT); //env
 
 // post images API
 router.post('/', upload, async (req, res) => {
