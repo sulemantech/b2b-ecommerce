023866000879ead0814ba3d6b298134ec082ca13
Sahibzada Hamza sardar <71062903+hamzoo124@users.noreply.github.com>
2024-03-07 12:49:18 +0500

    search API changes

--- client/src/store/slices/ShopSearch-Action.js
+++ client/src/store/slices/ShopSearch-Action.js
@@ -1,30 +1,12 @@
-// import { setSearchResults, setError, setLoading } from '../slices/ShopSearch-Slice';
 
-// import { setSearchResults, setError, setLoading } from '../slices/ShopSearch-Slice'; 
 
-// export const searchProducts = (searchTerm) => async (dispatch) => {
-//   try {
-//     dispatch(setLoading(true));
-
-//     const response = await fetch(`http://localhost:5001/api/product/`);
-//     const data = await response.json();
-//     dispatch(setSearchResults(data));
-//     console.log("searchhhhhhhhhhhhhhhhhhhhhhhhh",data);
-//   } catch (error) {
-//     console.error('Error during search:', error);
-//     dispatch(setError('An error occurred during the search. Please try again.'));
-//   } finally {
-//     dispatch(setLoading(false));
-//   }
-// };
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 import { setSearchResults, setError, setLoading } from '../slices/ShopSearch-Slice';
 
 export const searchProducts = (searchTerm,e) => async (dispatch) => {
   try {
     dispatch(setLoading(true));
 
-    const response = await fetch(`http://localhost:5001/api/product/`, {
+    const response = await fetch(`${process.env.REACT_APP_PUBLIC_URL}/api/product/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
