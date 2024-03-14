862411b01f9a58d15b5521fbc21ba9a90241505b
Sahibzada Hamza sardar <71062903+hamzoo124@users.noreply.github.com>
2024-03-14 14:39:47 +0500

    responsiveness working on mobile screen and add loader on siging

--- admin/src/components/Sidebar.tsx
+++ admin/src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { NavLink, useLocation } from 'react-router-dom';
 import SidebarLinkGroup from './SidebarLinkGroup';
 import { IoMdPricetag } from "react-icons/io";
 // import { useNavigate } from "react-router-dom";
-import Cookies from 'js-cookie';
+
 import './styl.css';
 
 interface SidebarProps {
@@ -15,7 +15,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   const location = useLocation();
   const { pathname } = location;
   // const navigate = useNavigate();
-  const isLogin = Cookies.get('token');
+  
 
   const trigger = useRef<any>(null);
   const sidebar = useRef<any>(null);
@@ -60,22 +60,40 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
     }
   }, [sidebarExpanded]);
 
-  const handleLogout = (): void => {
-    Cookies.remove('token');
-    Cookies.remove('role');
-    console.log('logout session');
-  };
 
   return (
     <aside
     style={{backgroundColor:"lightgray"}}
       ref={sidebar}
-      className={` left-0 top-0  border-10 flex h-screen  w-72.5 flex-col overflow-y-hidden  duration-300 ease-linear lg:static lg:translate-x-0 ${
+      className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
         sidebarOpen ? 'translate-x-0' : '-translate-x-full'
       }`}
     >
-     
 
+<div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
+        <button
+          ref={trigger}
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+          aria-controls="sidebar"
+          aria-expanded={sidebarOpen}
+          className="block lg:hidden"
+        >
+          <svg
+            className="fill-current"
+            width="20"
+            height="18"
+            viewBox="0 0 20 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
+              fill=""
+            />
+          </svg>
+        </button>
+      </div>
+     
       <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
         <nav className="px-4 lg:mt-9 lg:px-6">
           <div>
@@ -176,16 +194,16 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                           !open && 'hidden'
                         }`}
                       >
-                        <ul className="mb-5.5 flex flex-col gap-2.5 pl-6">
+                        <ul className="mb-5.5 flex flex-col gap-2.5 pl-6 ">
                           <li>
                             <div className="group relative flex items-center gap-2.5 rounded-md px-4 font-medium
                              text-black duration-300 ease-in-out hover:bg-bodydark1">
                              <IoMdPricetag />
                              <h1>
                              Products
-
                              </h1>
                             </div>
+                            </li>
                             <li >
                               <NavLink
                                 to="/product"
@@ -252,9 +270,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                                 Order
                               </NavLink>
                             </li>
-                          </li>
                         </ul>
-                        <ul>
+
+                        <ul className='hidden'>
                           <li>
                             <div className=" group relative flex items-center gap-2.5 rounded-md px-4 font-medium ml-4
                              text-black duration-300 ease-in-out hover:bg-bodydark1">
